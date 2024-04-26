02e4740f90417f79448dcb26ad41821465d35205
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2024-04-26 15:06:49 +0100

    nextSong

--- script.js
+++ script.js
@@ -112,10 +112,11 @@ const playNextSong = () => {
   if (userData?.currentSong === null) {
     playSong(userData?.songs[0].id);
   } else {
-    const currentSongIndex = getCurrentSongIndex()
+    const currentSongIndex = getCurrentSongIndex();
+    const nextSong = userData?.songs[currentSongIndex + 1];
+    playSong(nextSong.id); 
   }
-
-}
+};
 
 const renderSongs = (array) => {
   const songsHTML = array
